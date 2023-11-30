d1456506c7eb51f6db53e31250195361b614c114
khatritikam <khatritikam2597@gmail.com>
2023-11-30 16:05:40 +0530

    error handling

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import * as express from 'express'
 import { getEnvironmentVariables } from './environments/env';
 import mongoose from 'mongoose';
 import UserRouter from './routers/UserRouter';
+import bodyParser = require('body-parser');
+
 
 export class Server {
     public app: express.Application = express();
@@ -9,10 +11,13 @@ export class Server {
     constructor() {
         this.setConfigurations();
         this.setRoutes()
+        this.error404Handler()
+        this.handleErrors()
     }
 
     setConfigurations() {
         this.setMongodb()
+        this.configureBodyParser()
     }
 
     setMongodb(){
@@ -22,7 +27,30 @@ export class Server {
         })
     }
 
+    configureBodyParser(){
+        this.app.use(bodyParser.urlencoded({extended:true}))
+    }
+
     setRoutes(){
         this.app.use('/api/user',UserRouter);
+    }
+
+    error404Handler(){
+        this.app.use((req, res) =>{
+            res.status(404).json({
+                message:'Not Found',
+                status_code:404
+            });
+        })
+    }
+
+    handleErrors(){
+        this.app.use((error,req,res,next) => {
+            const errorStatus = req.errorStatus || 500;
+            res.status(errorStatus).json({
+                message:error.message || 'Something Went Wrong. Please Try Again',
+                status_code:errorStatus
+            })
+        })
     }
 }
